0e352c5523b0fc183f63abd1bb2ddfe9d672c741
Sylver Yagi <silvery4543@gmail.com>
2024-06-23 12:15:19 +1000

    package initialized

---
+++ lib/gad_banner_package.dart
@@ -0,0 +1,3 @@
+library gad_banner_package;
+
+export 'src/adbannercode.dart';
---
+++ test/gad_banner_package_test.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:gad_banner_package/gad_banner_package.dart';
+
+void main() {
+  // test('adds one to input values', () {
+  //   final adbanner = Adbannercode.createBannerAd();
+  //   final adbannerFluid = Adbannercode.createFluidBannerAd();
+  //   final adbannerFull = Adbannercode.createFullBannerAd();
+  //   final adbannerLarge = Adbannercode.createLargeBannerAd();
+  //   expect(adbanner);
+  //   expect(adbanner.addOne(-7), -6);
+  //   expect(adbanner.addOne(0), 1);
+  // });
+}
